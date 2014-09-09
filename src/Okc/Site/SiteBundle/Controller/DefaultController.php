c03b67a81cf79737395f0cb39ae738dfcf09e45c
yann <yann.boisselier@gmail.com>
2014-09-10 00:55:18 +0200

    add http headers on homepage

--- src/Okc/Site/SiteBundle/Controller/DefaultController.php
+++ src/Okc/Site/SiteBundle/Controller/DefaultController.php
@@ -3,11 +3,15 @@
 namespace Okc\Site\SiteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('OkcSiteSiteBundle:Default:homepage.html.twig');
+      $response = $this->render('OkcSiteSiteBundle:Default:homepage.html.twig');
+      $response->setETag(md5($response->getContent()));
+      $response->setPublic(); // permet de s'assurer que la réponse est publique, et qu'elle peut donc être cachée
+      return $response;
     }
 }
